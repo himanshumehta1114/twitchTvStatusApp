b5d66fa67179a28deaa06fee99417f2b074d26f0
himanshu <himanshumehta1114@gmail.com>
2017-09-13 20:01:52 +0530

    fixed status

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 		
 		var usernames = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "habathcx", "RobotCaleb", "noobs2ninjas"];
-		var status;
+		var base = 'https://wind-bow.glitch.me/twitch-api/';
+		var type = 'channels';
+		var url = function(base,type,user){
+				return base + type + "/" + user + "?callback=?";
+		};
+		
 		$.each(usernames,function(i){
-			var url = 'https://wind-bow.gomix.me/twitch-api/streams/' + usernames[i] + '?callback=?';
-			$.getJSON(url,function(data){
-				if(data.stream === null){
+			getUrl = url(base,type,usernames[i]);
+			$.getJSON(getUrl,function(data){
+				console.log(data);
+			
+			streamUrl = url(base,'streams',usernames[i]);
+			$.getJSON(streamUrl,function(streamData){
+				if(streamData.stream === null){
 					status = "offline";
-				}else if(data.stream === undefined){
-					status = "offline";
+					$("#tablebody").append("<tr><td class='offline'><a href='" + data.url +"'><img class='logo' src='" + data.logo + "' alt='channel logo' ></a></td><td><a class='name' href='"+data.url + "'>" +data.display_name+ "</a></td><td><a class='name' href='" +data.url+ "'>" + status +"</a></td></tr>");
+				}else if(streamData.stream === undefined){
+					status = "offline"
+					$("#tablebody").append("<tr><td class='offline'><a href='" + data.url +"'><img class='logo' src='" + data.logo + "' alt='channel logo' ></a></td><td><a class='name' href='"+data.url + "'>" +data.display_name+ "</a></td><td><a class='name' href='" +data.url+ "'>" + status +"</a></td></tr>");
 				}else{
-					console.log(data.stream.game);
-					status = data.stream.game;
+					status = streamData.stream.game;
+					$("#tablebody").append("<tr><td class='online'><a href='" + data.url +"'><img class='logo' src='" + data.logo + "' alt='channel logo' ></a></td><td><a class='name' href='"+data.url + "'>" +data.display_name+ "</a></td><td><a class='name' href='" +data.url+ "'>" + status +"</a></td></tr>");
 				}
+				});
 			});
-
-			url = 'https://wind-bow.gomix.me/twitch-api/channels/' + usernames[i] + '?callback=?';
-			$.getJSON(url, function(data) {
-				if(status !== "offline"){
-  				$("#tablebody").append("<tr><td class='online'><a href='" + data.url +"'><img class='logo' src='" + data.logo + "' alt='channel logo' ></a></td><td><a class='name' href='"+data.url + "'>" +data.display_name+ "</a></td><td><a class='name' href='" +data.url+ "'>" + status +"</a></td></tr>");
-  			console.log(data.display_name + status);}else{
-  				$("#tablebody").append("<tr><td class='offline'><a href='" + data.url +"'><img class='logo' src='" + data.logo + "' alt='channel logo' ></a></td><td><a class='name' href='"+data.url + "'>" +data.display_name+ "</a></td><td><a class='name' href='" +data.url+ "'>" + status +"</a></td></tr>");
-  			console.log(data.display_name + status);
-  			}
-  			});
-  			
-  			
   		});
 		
