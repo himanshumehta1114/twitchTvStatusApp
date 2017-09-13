a61c8e8a228534933fb918e6e0128b74cfe00d64
himanshu <himanshumehta1114@gmail.com>
2017-09-13 14:58:05 +0530

    Added channel names

--- app.js
+++ app.js
@@ -4,8 +4,8 @@
 		$.each(usernames,function(i){
 			var url = 'https://wind-bow.gomix.me/twitch-api/channels/' + usernames[i] + '?callback=?';
 			$.getJSON(url, function(data) {
+  				$("#tablebody").append("<tr><td><a href='" + data.url +"'><img class='logo' src='" + data.logo + "' alt='channel logo' ></a></td><td><a class='name' href='"+data.url + "'>" +data.display_name+ "</a></td></tr>");
   			console.log(data);});
 
-  			
-		});
+  		});
 		
