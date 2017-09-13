b57f22f515175bc2f251e15195f95cd02f06166f
himanshu <himanshumehta1114@gmail.com>
2017-09-13 20:22:37 +0530

    fixed status buttons

--- app.js
+++ app.js
@@ -15,15 +15,29 @@
 			$.getJSON(streamUrl,function(streamData){
 				if(streamData.stream === null){
 					status = "offline";
-					$("#tablebody").append("<tr><td class='offline'><a href='" + data.url +"'><img class='logo' src='" + data.logo + "' alt='channel logo' ></a></td><td><a class='name' href='"+data.url + "'>" +data.display_name+ "</a></td><td><a class='name' href='" +data.url+ "'>" + status +"</a></td></tr>");
+					$("#tablebody").append("<tr class='offline'><td><a href='" + data.url +"'><img class='logo' src='" + data.logo + "' alt='channel logo' ></a></td><td><a class='name' href='"+data.url + "'>" +data.display_name+ "</a></td><td><a class='name' href='" +data.url+ "'>" + status +"</a></td></tr>");
 				}else if(streamData.stream === undefined){
 					status = "offline"
-					$("#tablebody").append("<tr><td class='offline'><a href='" + data.url +"'><img class='logo' src='" + data.logo + "' alt='channel logo' ></a></td><td><a class='name' href='"+data.url + "'>" +data.display_name+ "</a></td><td><a class='name' href='" +data.url+ "'>" + status +"</a></td></tr>");
+					$("#tablebody").append("<tr class='offline'><td class='offline'><a href='" + data.url +"'><img class='logo' src='" + data.logo + "' alt='channel logo' ></a></td><td><a class='name' href='"+data.url + "'>" +data.display_name+ "</a></td><td><a class='name' href='" +data.url+ "'>" + status +"</a></td></tr>");
 				}else{
 					status = streamData.stream.game;
-					$("#tablebody").append("<tr><td class='online'><a href='" + data.url +"'><img class='logo' src='" + data.logo + "' alt='channel logo' ></a></td><td><a class='name' href='"+data.url + "'>" +data.display_name+ "</a></td><td><a class='name' href='" +data.url+ "'>" + status +"</a></td></tr>");
+					$("#tablebody").append("<tr class='online'><td class='online'><a href='" + data.url +"'><img class='logo' src='" + data.logo + "' alt='channel logo' ></a></td><td><a class='name' href='"+data.url + "'>" +data.display_name+ "</a></td><td><a class='name' href='" +data.url+ "'>" + status +"</a></td></tr>");
 				}
 				});
 			});
   		});
+		$(document).ready(function(){
+			$("#all").click(function(){
+				$(".online,.offline").removeClass("hidden");
+			});
+			$("#online").click(function(){
+				$(".offline").addClass("hidden");
+				$(".online").removeClass("hidden");
+			});
+			$("#offline").click(function(){
+				$(".online").addClass("hidden");
+				$(".offline").removeClass("hidden");
+			})
+		});
+  		
 		
