1dc79d997e304af4806b1c6d6b427fc6b2544f3a
himanshu <himanshumehta1114@gmail.com>
2017-09-13 15:03:02 +0530

    added status

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 		
-		var usernames = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+		var usernames = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
 		$.each(usernames,function(i){
 			var url = 'https://wind-bow.gomix.me/twitch-api/channels/' + usernames[i] + '?callback=?';
 			$.getJSON(url, function(data) {
-  				$("#tablebody").append("<tr><td><a href='" + data.url +"'><img class='logo' src='" + data.logo + "' alt='channel logo' ></a></td><td><a class='name' href='"+data.url + "'>" +data.display_name+ "</a></td></tr>");
+  				$("#tablebody").append("<tr><td><a href='" + data.url +"'><img class='logo' src='" + data.logo + "' alt='channel logo' ></a></td><td><a class='name' href='"+data.url + "'>" +data.display_name+ "</a></td><td><a href='" +data.url+ "'>" + data.status +"</a></td></tr>");
   			console.log(data);});
 
   		});
